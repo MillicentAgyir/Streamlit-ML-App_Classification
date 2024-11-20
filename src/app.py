1a73710750eecb20ad0ea4a79269a9af6417531f
Millicent Agyir <Millicent.agyir@azubiafrica.org>
2024-11-20 09:44:39 +0000

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
  
-st.write("""pip install -r requirements.txt
-# My first app
+st.write("""# Telco Customer Churn Prediction
 Hello *world!*
 """)
-sepal_height = st.number_input("Ënter sepal height")
-sepal_width = st.number_input("Ënter sepal width")
+sepal_height = st.number_input("Enter sepal height")
+sepal_width = st.number_input("Enter sepal width")
 
-petal_height = st.number_input("Ënter petal height")
-petal_width = st.number_input("Ënter petal width") 
+petal_height = st.number_input("Enter petal height")
+petal_width = st.number_input("Enter petal width") 
 
 if st.button("Predict"):
     st.text(f"The iris has been classified as:  '{''}'.")
